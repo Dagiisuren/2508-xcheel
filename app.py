b527784d215dc9e8fcbfb5ed2d061d94fe810856
Dagiisuren <54223365+Dagiisuren@users.noreply.github.com>
2019-08-25 11:23:49 +0800

    xchedel
    08025

---
+++ app.py
@@ -0,0 +1,92 @@
+# # a = input()
+# # b = input()
+# # ih = max(a,b)
+# # print(ih)
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     ih = a
+# else:
+#     ih = b
+# print(ih)
+# bod 1
+# a = int(input())
+# if a > 100:
+#     print(True)
+# else:
+#     print(False)
+# bod 2
+# n = int(input())
+# if n % 3 == 0:
+#     print(True)
+# else:
+#     print(False)
+# bod 4
+# n = int(input())
+# if n > 0:
+#     print("Эерэг")
+# else:
+#     print("Сөрөг")
+# bod 3
+# n = int(input())
+# if n // 7 == 0:
+#     print(n)
+# else : 
+#     n % 7 != 0
+#     print(n % 7)
+# bod 6
+# n = int(input())
+# if n % 3 != 0:
+#     print(n % 3)
+# else:
+#     n // 3 == 0
+#     print(n // 3)
+# bod 7
+# n = int(input())
+# if n % 2 == 0 :
+#     print(True)
+# else:
+#     n % 2 != 0
+#     print(False)
+# bod 8
+# n = int(input())
+# a1 = n % 100
+# if a1 % 2 == 0:
+#     print(True)
+# else:
+#     a1 % 2 != 0
+#     print(False)
+# bod 9
+# n = input()
+# tom = n.upper()
+# print(tom)
+# bod 10
+# n = input()
+# too = len(n)
+# if too > 5:
+#     tom = n.upper()
+#     print(tom)
+# else:
+#     print(n)
+# bod 11
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = min(a,b,c)
+# print(d)
+# bod 12
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# x = max(a,b,c,d)
+# print(x)
+# bod 13
+
+# bod 14
+x1 ,y1 ,x2, y2 = [int(i) for i in input().split()]
+if x1 == x2 or y1 == y2:
+    print("yes")
+else:
+    print("no")
+
